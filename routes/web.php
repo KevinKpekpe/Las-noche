cbfc33c0c5eaec5b6ce120f6b94a6f630c98a800
Spectre <kpekpekevin57@gmail.com>
2023-05-31 16:37:05 +0100

    Second commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoomController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/',[RoomController::class,'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/room',[RoomController::class,'index']);
 Route::get('/room/{slug}-{room}',[RoomController::class,'show'])->where([
     "room" => '[0-9]+',
     "slug" => '[a-z0-9\-]+'
@@ -24,3 +27,14 @@ Route::get('/room/reservation/{slug}-{room}',[RoomController::class,'makereserva
     "room" => '[0-9]+',
     "slug" => '[a-z0-9\-]+'
 ])->name('reservation');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
